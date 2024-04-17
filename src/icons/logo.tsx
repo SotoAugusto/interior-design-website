094efd10b7ef511891fd9301661169b2831e3d55
Augusto Soto <sotoaugustoa@gmail.com>
2024-04-17 01:10:10 -0600

    add text company name

--- src/icons/logo.tsx
+++ src/icons/logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <p className="text-3xl font-bold text-center">PACER CONSULTING GROUP</p>
+    <p className="text-sm sm:text-xl md:text-2xl lg:text-3xl font-bold text-center">PACER CONSULTING GROUP</p>
   );
 };
 
